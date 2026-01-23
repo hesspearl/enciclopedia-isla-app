f1b54558bbf713d483b2165d0bbf0e0cd5c499a0
HessDev <hesspearldev@gmail.com>
2026-01-23 12:16:22 -0300

    develop with https

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs';
+import path from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +11,12 @@ export default defineConfig({
     ,
      tailwindcss(),
   ],
+    server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
+    },
+    port: 3000
+  }
+
 })
